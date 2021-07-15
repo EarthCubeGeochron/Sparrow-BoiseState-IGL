396283b1c0c262218494c7d8b3b16cbd3bea65d0
Daven Quinn <dev@davenquinn.com>
2021-07-15 16:42:10 -0600

    Added ICP importer that just lists data files

---
+++ data-importers/icp_importer/cli.py
@@ -0,0 +1,26 @@
+from os import environ
+from click import command, option, echo, secho, style
+from sparrow.context import get_database
+from pathlib import Path
+
+@command(name="import-icp-data")
+@option("--fix-errors", is_flag=True, default=False)
+@option("--redo", is_flag=True, default=False)
+def cli(**kwargs):
+    """
+    Import Boise State LA-ICP-MS data files
+    """
+    varname = "SPARROW_DATA_DIR"
+    env = environ.get(varname, None)
+    if env is None:
+        v = style(varname, fg="cyan", bold=True)
+        echo(f"Environment variable {v} is not set.")
+        secho("Aborting", fg="red", bold=True)
+        return
+    path = Path(env)
+    assert path.is_dir()
+
+    db = get_database()
+    files = path.glob("**/*.xlsx")
+    for file in files:
+        print(file)
