ddb86109ff4a7bfa5c81c6a259bdbb776fb96f8e
Daven Quinn <dev@davenquinn.com>
2021-10-06 18:01:05 -0500

    Small extension to ICP importer

--- plugins/icp_importer/__init__.py
+++ plugins/icp_importer/__init__.py
@@ -7,6 +7,27 @@ from rich import print
 import sparrow
 
 
+def convert_to_json(df):
+    """
+    Convert a feather file to JSON for Sparrow import.
+    """
+    # Create a sample column that eliminates the last indexing suffix of the analysis column,
+    # while saving that suffix in another column. Uses regex. See https://regex101.com/r/ECVDtm/1
+    split_samplename = df.loc[:, "analysis"].str.extract(r"^(.+)(_| )(\d+)$")
+    print(split_samplename)
+    df["sample"] = split_samplename.iloc[:, 0]
+
+    # Maybe we also want to split grainsize fractions here? Can use a similar regex.
+
+    df["session_index"] = split_samplename.iloc[:, 2]
+
+    groups = df.groupby("sample")
+    print(f"Grouped data into {len(groups)} samples.")
+
+    for ix, group in groups:
+        print(ix, len(group))
+
+
 @sparrow.task()
 def import_icp_data(fix_errors: bool = False, redo: bool = False):
     """
@@ -26,5 +47,6 @@ def import_icp_data(fix_errors: bool = False, redo: bool = False):
     files = path.glob("**/*.feather")
     for file in files:
         df = read_feather(file)
+        convert_to_json(df)
         print(df)
     print("Done")
